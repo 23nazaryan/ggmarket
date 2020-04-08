cfe9aaf7edd5da09dc39a84b714c0c695969c475
23nazaryan@gmail.com <1f8d2ea4d9>
2020-04-08 16:39:18 +0400

    add contact email

--- keys/keys.prod.js
+++ keys/keys.prod.js
@@ -3,7 +3,8 @@ module.exports = {
     SESSION_SECRET: process.env.SESSION_SECRET,
     BASE_URL: process.env.BASE_URL,
     EMAIL_FROM: process.env.EMAIL_FROM,
+    EMAIL_CONTACT: process.env.EMAIL_CONTACT,
     EMAIL_HOST: process.env.EMAIL_HOST,
     EMAIL_PORT: process.env.EMAIL_PORT,
-    EMAIL_PASS: process.env.EMAIL_PASS,
+    EMAIL_PASS: process.env.EMAIL_AUTH_PASS,
 }
