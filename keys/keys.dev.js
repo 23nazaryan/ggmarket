19387ba4004f1763e9a6c889aec4cd9957bb7f37
23nazaryan@gmail.com <1f8d2ea4d9>
2020-04-17 20:49:43 +0400

    set mail configs

--- keys/keys.dev.js
+++ keys/keys.dev.js
@@ -2,9 +2,9 @@ module.exports = {
     MONGODB_URI: 'mongodb+srv://ggmarket:mSKEihVjgK9Mmd98@cluster0-mglgq.mongodb.net/market',
     SESSION_SECRET: 'some secret value',
     BASE_URL: 'https://ggmarket.am',
-    EMAIL_HOST: 'mail.hostland.ru',
+    EMAIL_HOST: 'smtp.mail.ru',
     EMAIL_PORT: 465,
-    EMAIL_FROM: 'mail@ggmarket.am',
-    EMAIL_PASS: 'mSKEihVjgK9Mmd98',
+    EMAIL_FROM: 'ggmarket2@mail.ru',
+    EMAIL_PASS: '115644gg',
     EMAIL_CONTACT: 'ggmarket1@mail.ru'
 }
